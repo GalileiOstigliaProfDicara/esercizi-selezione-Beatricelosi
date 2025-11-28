df1d5d9453fa9bff3e372f3e7f32bc3c74b0b6b2
Beatricelosi <beatricediamante.losi@galileiostiglia.edu.it>
2025-11-28 10:57:25 +0000

    es2.c

--- es1.c
+++ es1.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 
 int main(){
-
+    int num1, num2;
+    printf("definisci numero 1: \n");
+    scanf("%d", &num1);
+    printf("definisci numero 2: \n");
+    scanf("%d", &num2);
+    if(num1 > num2)
+    {
+        printf("%d è maggiore di %d", num1, num2);
+    }
+    else if (num1 < num2)
+    {
+        printf("%d è minore di %d", num1, num2);
+    }
+    else
+    printf("%d è uguale a %d", num1, num2);
     return 0;
 }
--- es2.c
+++ es2.c
@@ -1,8 +1,22 @@
-/*L’utente inserisce la propria età e il programma dice se è maggiorenne (ovvero con età maggiore uguale a 18 anni)*/
+/*L’utente inserisce la propria età e il programma dice se è maggiorenne
+ (ovvero con età maggiore uguale a 18 anni)*/
 
 #include <stdio.h>
 
 int main(){
+int età;
+printf("inserisci la tua età:");
+scanf("%d", &età);
+if(età > 18)
+    {
+        printf("%d è maggiore di 18", età);
+    }
+    else if (età < 18)
+    {
+        printf("%d è minore di 18", età);
+    }
+    else
+    printf("%d è uguale a 18", età);
 
     return 0;
 }
